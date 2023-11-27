60d60372eebf5da72a07af0f89402b3f89dcc433
Mustafa <mustafa.abdalla@tu-clausthal.com>
2023-11-28 00:29:40 +0100

    Food collision, Snake in grid, and snake growth

--- Optimised/game.py
+++ Optimised/game.py
@@ -19,7 +19,7 @@ pygame.display.set_caption('Optimised Path Snake')
 clock = pygame.time.Clock()
 
 # Grid setup
-resolution = 25
+resolution = 20
 cols = WIDTH // resolution
 rows = HEIGHT // resolution
 
@@ -27,11 +27,12 @@ grid = []
 
 # Snake setup
 snake = Snake()
-snake.x = 500
-snake.y = 500
-snake.WIDTH = resolution
-snake.HEIGHT = resolution
-snake_rect = pygame.Rect(snake.x, snake.y, snake.WIDTH, snake.HEIGHT)
+snake.x = 10
+snake.y = 10
+snake.WIDTH = 1
+snake.HEIGHT = 1
+snake.length = 1
+snake_rect = pygame.Rect(snake.x*resolution, snake.y*resolution, snake.WIDTH*resolution, snake.HEIGHT*resolution)
 
 # Snake movement
 snake.x_vel = 0
@@ -39,8 +40,10 @@ snake.y_vel = 0
 
 # Food
 food = Food()
-food.x = random.randint(0, cols)
-food.y = random.randint(0, rows)
+food.x = random.randint(0, cols-1)
+food.y = random.randint(0, rows-1)
+food.WIDTH = 1
+food.HEIGHT = 1
 
 def draw_grid(grid):
     # Fields of grid
@@ -48,9 +51,10 @@ def draw_grid(grid):
         for row in range(len(grid)):
             if grid[row][col] == 0:
                 pygame.draw.rect(window, (65,65,65), pygame.Rect(col*resolution, row*resolution, resolution, resolution))
-            else:
-                pygame.draw.rect(window, (255,0,0), pygame.Rect(col*resolution, row*resolution, resolution, resolution))
-
+            elif grid[row][col] == -1:
+                pygame.draw.rect(window, (255,0,0), pygame.Rect(col*resolution, row*resolution, food.WIDTH*resolution, food.HEIGHT*resolution))
+            elif grid[row][col] >= 1:
+                pygame.draw.rect(window, (255,255,255), pygame.Rect(col*resolution, row*resolution, snake.WIDTH*resolution, snake.HEIGHT*resolution))
 
     # Grid lines
     for col in range(0, WIDTH, resolution):
@@ -63,23 +67,23 @@ def fill_grid(cols: int, rows: int):
     return [[0 for col in range(cols)] for row in range(rows)]
 
 def move_right(snake:Snake) -> int:
-    snake.x_vel = resolution
+    snake.x_vel = 1
     return snake.x_vel
 
 def move_left(snake:Snake) -> int:
-    snake.x_vel = -resolution
+    snake.x_vel = -1
     return snake.x_vel
 
 def move_up(snake:Snake) -> int:
-    snake.y_vel = -resolution
+    snake.y_vel = -1
     return snake.y_vel
 
 def move_down(snake:Snake) -> int:
-    snake.y_vel = resolution
+    snake.y_vel = 1
     return snake.y_vel
 
 def check_border_collison(snake:Snake) -> bool:
-    if snake.x > WIDTH:
+    if snake.x + snake.WIDTH > WIDTH:
         return True
         # snake.x_vel = 0
         # snake.x = WIDTH -snake.WIDTH
@@ -87,7 +91,7 @@ def check_border_collison(snake:Snake) -> bool:
         return True
         # snake.x_vel = 0
         # snake.x = 0
-    if snake.y > HEIGHT:
+    if snake.y + snake.HEIGHT > HEIGHT:
         return True
         # snake.y_vel = 0
         # snake.y = HEIGHT - snake.HEIGHT
@@ -95,6 +99,26 @@ def check_border_collison(snake:Snake) -> bool:
         return True
         # snake.y_vel = 0
         # snake.y = 0 
+
+def food_collision(snake:Snake, food:Food) -> bool:
+    left_collision, right_collision, top_collision, bottom_collision = False, False, False, False
+    if (snake.x + snake.WIDTH > food.x):
+        right_collision = True
+    if (snake.x < food.x + food.WIDTH):
+        left_collision = True
+    if (snake.y + snake.HEIGHT > food.y):
+        bottom_collision = True
+    if (snake.y < food.y + food.HEIGHT):
+        top_collision = True
+
+    if (left_collision and right_collision and bottom_collision and top_collision):
+        return True
+    
+def remove_tail(grid):
+    for col in range(len(grid[0])):
+        for row in range(len(grid)):
+            if grid[row][col] > 0:
+                grid[row][col] -= 1 
 
 def main(snake:Snake, grid):
 
@@ -122,6 +146,10 @@ def main(snake:Snake, grid):
             snake.x_vel = 0
             snake.y_vel = move_down(snake)
         
+        # Filling grid array and drawing it onto window
+        if not grid:
+            grid = fill_grid(cols, rows)
+
         # Snake movement
         snake.x += snake.x_vel
         snake.y += snake.y_vel
@@ -130,24 +158,36 @@ def main(snake:Snake, grid):
         if (check_border_collison(snake)):
             print('GAME OVER')
             sys.exit(0)
+        else:
+            grid[snake.y][snake.x] = 1 + snake.length
+            remove_tail(grid)
+
+        # Checking collision with food
+        if (food_collision(snake, food)):
+            snake.length += 1
+            grid[food.y][food.x] = 0
+            food.x = random.randint(0, cols-1)
+            food.y = random.randint(0, rows-1)
 
         window.fill((65,65,65))
 
-        # Filling grid array and drawing it onto window
-        if not grid:
-            grid = fill_grid(cols, rows)
+        # # Filling grid array and drawing it onto window
+        # if not grid:
+        #     grid = fill_grid(cols, rows)
 
         draw_grid(grid)
 
         # Randomly placing the food in the grid and having it be drawn
         grid[food.y][food.x] = -1
 
-        snake_rect = pygame.Rect(snake.x,snake.y,snake.WIDTH,snake.HEIGHT)
-        snake_rect_draw = pygame.draw.rect(window, (255,255,255), snake_rect)
+        grid[snake.y][snake.x] = snake.length
+
+        # snake_rect = pygame.Rect(snake.x*resolution,snake.y*resolution,snake.WIDTH*resolution,snake.HEIGHT*resolution)
+        # snake_rect_draw = pygame.draw.rect(window, (255,255,255), snake_rect)
 
         pygame.display.update()
 
-        clock.tick(30)
+        clock.tick(15)
 
 
 main(snake, grid)
--- Optimised/snake.py
+++ Optimised/snake.py
@@ -6,3 +6,4 @@ class Snake:
     y_vel: int = 0
     WIDTH: int = 0
     HEIGHT: int = 0
+    length: int = 0
